81f14f380653d100efd59e9c6e8e3b4cb54bd545
JorisB <jorisbudin@gmail.com>
2024-07-03 16:10:54 +0200

    create route

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import { Outlet } from "react-router-dom";
 import "./App.css";
-import Accueil from "./page/Accueil/Accueil";
 
 function App() {
   return (
--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,16 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import Asso from "./page/asso/Asso.jsx";
+import Accueil from "./page/Accueil/Accueil.jsx";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
-    children: [{ path: "/asso", element: <Asso /> }],
+    children: [
+      { path: "/", element: <Accueil /> },
+      { path: "/asso", element: <Asso /> },
+    ],
   },
 ]);
 
