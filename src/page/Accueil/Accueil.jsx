ac64286c45a4811dacdc7e11e66cb1575316ae8f
Alp Hamurcu <alp_hamurcu@yahoo.com>
2024-07-03 15:58:45 +0200

    page accueil

--- src/page/Accueil/Accueil.jsx
+++ src/page/Accueil/Accueil.jsx
@@ -1,10 +1,22 @@
 import "./Accueil.css";
+import logoImage from "../../assets/vieudetour1.png";
 
 function Accueil() {
   return (
-    <div className="logo">
-      <img src="./src/assets/vieudetour1.png" alt="ss" />
-    </div>
+    <main className="accueil">
+      <div className="logo-container">
+        <img className="logo" src={logoImage} alt="logo" />
+        <h1>AIDE UN VIOQUE</h1>
+      </div>
+      <div className="button">
+        <button className="button1" type="button">
+          Associations
+        </button>
+        <button className="button2" type="button">
+          Informations
+        </button>
+      </div>
+    </main>
   );
 }
 
