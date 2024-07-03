080432f1a0e15e79ae8b7de40de903d5b2745c30
JorisB <jorisbudin@gmail.com>
2024-07-03 15:58:13 +0200

    Asso page

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,21 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Asso from "./page/asso/Asso.jsx";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [{ path: "/asso", element: <Asso /> }],
+  },
+]);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
---
+++ src/page/asso/Asso.jsx
@@ -0,0 +1,49 @@
+import styles from "./Asso.module.css";
+import logoAsso from "../../assets/vielle.png";
+import phone from "../../assets/phone.png";
+import boxMail from "../../assets/boite.png";
+
+function Asso() {
+  const association = {
+    name: "Association les copains des vieux",
+    phone: "06 00 00 00 00",
+    address: "2 rue du repos 69008 Lyon",
+    description:
+      "Ces braves gens vont t’aider à affronter l'internets vieille branche, ils sachent",
+  };
+
+  return (
+    <>
+      <div className={styles.assoContainer}>
+        <img
+          className={styles.logoAsso}
+          src={logoAsso}
+          alt="logo de l'association"
+        />
+        <h2 className={styles.name}>{association.name}</h2>
+        <div className={styles.container}>
+          <img
+            src={phone}
+            className={styles.phonePicture}
+            alt="icone téléphone"
+          ></img>
+          <p> {association.phone}</p>
+        </div>
+        <div className={styles.container}>
+          <img
+            className={styles.boxMailPicture}
+            src={boxMail}
+            alt="icone de boite au lettre"
+          ></img>
+          <p>{association.address}</p>
+        </div>
+        <div className={styles.descriptionBox}>
+          <span className={styles.descriptionTitle}>Description :</span>
+          <p className={styles.description}>{association.description}</p>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Asso;
